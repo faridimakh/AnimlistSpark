9207b0285dd4f71c96e9a84a13606399e3769ac2
faridimakh <secintfar1A>
2019-11-28 22:33:51 +0100

    end commit

--- src/main/scala/tools/fonctions/shell_process.scala
+++ src/main/scala/tools/fonctions/shell_process.scala
@@ -7,5 +7,5 @@ import scala.sys.process._
 object shell_process {
   Process("chmod 777 src/main/resources/script.sh").!
   lazy val check_configuration_running: Int = Seq("./src/main/resources/script.sh", data_path, path_queries_to_process, path_query_for_storage).!!.trim.toInt
-  lazy val clean_Queries: Int =Process("find /home/farid/Bureau/requettodellet/ -not -name *.csv -type f -delete").!
+  lazy val clean_Queries: Int = Seq("find" ,path_query_for_storage, "-not","-name","*.csv", "-type","f","-delete").!
 }
