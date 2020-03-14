22ba3618da4b1f593e8a4e87db57cc9883d9c638
farid <imakhfarid@gmail.com>
2020-03-14 21:50:31 +0100

    "orthographe"

--- src/main/scala/mainanimelist.scala
+++ src/main/scala/mainanimelist.scala
@@ -7,8 +7,8 @@ object mainanimelist {
     spark.sparkContext.setLogLevel(myconf.getString("spark.stop_log"))
     if (check_configuration_running) {
       cleaner.run()
-      println("les requettes stockées dans le repertoire \n" + queries_path +
-        "\n sont lancées sur les données stockées dans " + "\n" + data_csv_path + "...\n  \n...")
+      println("queries stored in the directory \n" + queries_path +
+        "\n are lunched for processing data located in " + "\n" + data_csv_path + "...\n  \n...")
       run_and_stock_my_quries()
       println("success! get your results in the following repository: \n"
         + queries_result_path)
